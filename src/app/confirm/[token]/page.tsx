a48589412062ce204e27c9238f5a969aec2a70a4
Pratham16112002 <prathamdhiman@rediffmail.com>
2025-01-05 11:27:27 +0000

    Small fix

---
+++ src/app/confirm/[token]/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { Button, Box, Typography, Container } from "@mui/material";
+import { styled } from "@mui/system";
+import { FaArrowRight } from "react-icons/fa";
+import React from "react";
+import { notFound, redirect } from "next/navigation";
+
+const WelcomeContainer = styled(Box)(({}) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "80vh",
+  textAlign: "center",
+}));
+
+const ConfirmPage = async ({
+  params,
+}: {
+  params: Promise<{ token: string }>;
+}) => {
+  const token = (await params).token;
+
+  if (!token) {
+    return notFound;
+  }
+
+  const activateHandler = async () => {
+    fetch(`/api/confirm/${token}`, {
+      method: "PUT",
+    });
+    redirect("/user/login");
+  };
+
+  return (
+    <Container>
+      <WelcomeContainer>
+        <Typography
+          component={"h2"}
+          variant="h2"
+          gutterBottom
+          sx={{ color: "#2c3e50", fontWeight: "bold" }}
+        >
+          Click here to account activate
+        </Typography>
+        <Typography
+          component={"h5"}
+          variant="h5"
+          gutterBottom
+          sx={{ color: "#666", mb: 4 }}
+        >
+          Your journey to amazing content begins here
+        </Typography>
+        <Button
+          onClick={activateHandler}
+          variant="contained"
+          size="large"
+          endIcon={<FaArrowRight />}
+          sx={{
+            backgroundColor: "#2c3e50",
+            padding: "12px 30px",
+            fontSize: "1.1rem",
+            "&:hover": {
+              backgroundColor: "#34495e",
+            },
+          }}
+        >
+          Login Now
+        </Button>
+      </WelcomeContainer>
+    </Container>
+  );
+};
+
+export default ConfirmPage;
--- src/app/confirm/[token]/route.ts
+++ src/app/confirm/[token]/route.ts
@@ -1,7 +1,7 @@
 import axiosConfig from "../../../axios/axiosConfig";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function handler(
+export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ token: string }> }
 ) {
