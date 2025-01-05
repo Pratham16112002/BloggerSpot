cb683238474c13e349a43c6bc59a3f6cd80f0fcd
Pratham16112002 <prathamdhiman@rediffmail.com>
2025-01-05 10:43:43 +0000

    confirm fix

--- src/app/confirm/[token]/route.ts
+++ src/app/confirm/[token]/route.ts
@@ -1,15 +1,18 @@
 import axiosConfig from "../../../axios/axiosConfig";
-import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ token: string }> }
 ) {
   const token = (await params).token;
-  if ((await axiosConfig.put(`users/activate/${token}`)).status == 204) {
-    redirect("/confirm");
-  } else {
-    redirect("/");
+  const url = request.nextUrl;
+  try {
+    await axiosConfig.put(`users/activate/${token}`);
+    url.pathname = "/confirm";
+    return NextResponse.redirect(url);
+  } catch {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
   }
 }
