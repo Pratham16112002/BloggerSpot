d9aee853985b5b95cb2bb30446676584724bcd5d
Pratham16112002 <prathamdhiman@rediffmail.com>
2025-01-05 11:43:47 +0000

    :fix better fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/confirm/:token",
+        destination: "/confirm",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
